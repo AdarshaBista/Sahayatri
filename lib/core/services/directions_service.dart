285aedc0361cb6370b543338a67a5af1d9014b9c
AdarshaBista <adrsh07@gmail.com>
2020-06-08 22:00:29 +0545

    Temporarily removed directions service

--- lib/core/services/directions_service.dart
+++ lib/core/services/directions_service.dart
@@ -1,6 +1,6 @@
 import 'package:meta/meta.dart';
 
-import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
+// import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 
 import 'package:sahayatri/core/models/place.dart';
 import 'package:sahayatri/core/models/failure.dart';
@@ -11,7 +11,7 @@ import 'package:sahayatri/core/services/location_service.dart';
 class DirectionsService {
   final LocationService locationService;
 
-  MapboxNavigation _directions;
+  // MapboxNavigation _directions;
 
   DirectionsService({
     @required this.locationService,
@@ -26,28 +26,29 @@ class DirectionsService {
   }
 
   Future<void> startNavigation(Place trailHead, UserLocation userLocation) async {
-    _directions = MapboxNavigation(onRouteProgress: (arrived) async {
-      if (arrived) await _directions.finishNavigation();
-    });
+    print('Directions from ${trailHead.coord} to ${userLocation.coord}');
+    // _directions = MapboxNavigation(onRouteProgress: (arrived) async {
+    //   if (arrived) await _directions.finishNavigation();
+    // });
 
-    final Location _origin = Location(
-      name: 'Your location',
-      latitude: userLocation.coord.lat,
-      longitude: userLocation.coord.lng,
-    );
+    // final Location _origin = Location(
+    //   name: 'Your location',
+    //   latitude: userLocation.coord.lat,
+    //   longitude: userLocation.coord.lng,
+    // );
 
-    final Location _destination = Location(
-      name: trailHead.name,
-      latitude: trailHead.coord.lat,
-      longitude: trailHead.coord.lng,
-    );
+    // final Location _destination = Location(
+    //   name: trailHead.name,
+    //   latitude: trailHead.coord.lat,
+    //   longitude: trailHead.coord.lng,
+    // );
 
-    await _directions.startNavigation(
-      origin: _origin,
-      destination: _destination,
-      language: 'en',
-      units: VoiceUnits.metric,
-      mode: NavigationMode.driving,
-    );
+    // await _directions.startNavigation(
+    //   origin: _origin,
+    //   destination: _destination,
+    //   language: 'en',
+    //   units: VoiceUnits.metric,
+    //   mode: NavigationMode.driving,
+    // );
   }
 }
--- lib/ui/pages/place_page/widgets/lodges_grid.dart
+++ lib/ui/pages/place_page/widgets/lodges_grid.dart
@@ -4,7 +4,6 @@ import 'package:sahayatri/core/models/place.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:sahayatri/ui/shared/animators/fade_animator.dart';
 import 'package:sahayatri/ui/pages/place_page/widgets/lodge_card.dart';
 
 class LodgesGrid extends StatelessWidget {
@@ -14,22 +13,20 @@ class LodgesGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     final lodges = Provider.of<Place>(context).lodges;
 
-    return FadeAnimator(
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 10 / 13,
-          crossAxisSpacing: 12.0,
-          mainAxisSpacing: 12.0,
-        ),
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-        itemCount: lodges.length,
-        itemBuilder: (context, index) {
-          return LodgeCard(lodge: lodges[index]);
-        },
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 10 / 13,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
       ),
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+      itemCount: lodges.length,
+      itemBuilder: (context, index) {
+        return LodgeCard(lodge: lodges[index]);
+      },
     );
   }
 }
