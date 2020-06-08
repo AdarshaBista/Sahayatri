827ab1765f8f80e025bdd6a24b5e1ce764ffeeb8
AdarshaBista <adrsh07@gmail.com>
2020-06-08 23:08:13 +0545

    Itinerary map now shows checkpoint markers

--- lib/ui/pages/itinerary_page/widgets/itinerary_map.dart
+++ lib/ui/pages/itinerary_page/widgets/itinerary_map.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'package:sahayatri/core/models/itinerary.dart';
-import 'package:sahayatri/core/models/checkpoint.dart';
 
 import 'package:provider/provider.dart';
 
 import 'package:flutter_map/flutter_map.dart';
-import 'package:sahayatri/ui/styles/styles.dart';
 import 'package:sahayatri/ui/shared/widgets/custom_map.dart';
-import 'package:sahayatri/ui/shared/widgets/place_marker.dart';
 import 'package:sahayatri/ui/pages/itinerary_page/widgets/checkpoint_marker.dart';
 
-class ItineraryMap extends StatefulWidget {
+class ItineraryMap extends StatelessWidget {
   const ItineraryMap();
-
-  @override
-  _ItineraryMapState createState() => _ItineraryMapState();
-}
-
-class _ItineraryMapState extends State<ItineraryMap> {
-  Checkpoint tappedCheckpoint;
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +20,6 @@ class _ItineraryMapState extends State<ItineraryMap> {
     return CustomMap(
       center: center,
       markerLayerOptions: _buildMarkers(context),
-      onTap: (_) => setState(() {
-        tappedCheckpoint = null;
-      }),
     );
   }
 
@@ -40,28 +27,17 @@ class _ItineraryMapState extends State<ItineraryMap> {
     final checkpoints = Provider.of<Itinerary>(context).checkpoints;
 
     return MarkerLayerOptions(
-      markers: [
-        for (int i = 0; i < checkpoints.length; ++i)
-          PlaceMarker(
-            point: checkpoints[i].place.coord.toLatLng(),
-            color: checkpoints[i] == tappedCheckpoint
-                ? AppColors.background
-                : AppColors.accentColors[i % AppColors.accentColors.length],
-            onTap: () {
-              setState(() {
-                tappedCheckpoint = checkpoints[i];
-              });
-            },
-          ),
-        if (tappedCheckpoint != null)
-          Marker(
-            width: 200,
-            height: 64,
-            anchorPos: AnchorPos.align(AnchorAlign.top),
-            point: tappedCheckpoint.place.coord.toLatLng(),
-            builder: (_) => CheckpointMarker(checkpoint: tappedCheckpoint),
-          ),
-      ],
+      markers: checkpoints
+          .map(
+            (c) => Marker(
+              width: 200,
+              height: 64,
+              anchorPos: AnchorPos.align(AnchorAlign.top),
+              point: c.place.coord.toLatLng(),
+              builder: (_) => CheckpointMarker(checkpoint: c),
+            ),
+          )
+          .toList(),
     );
   }
 }
