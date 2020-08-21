0e5bed9a3431b5764998d19fdeab0b2d072aca88
AdarshaBista <adrsh07@gmail.com>
2020-08-21 11:42:18 +0545

    Tracker button is only shown when places have been loaded

--- lib/ui/pages/destination_detail_page.dart/widgets/tracker_fab.dart
+++ lib/ui/pages/destination_detail_page.dart/widgets/tracker_fab.dart
@@ -6,6 +6,7 @@ import 'package:sahayatri/core/services/navigation_service.dart';
 import 'package:sahayatri/app/constants/routes.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sahayatri/cubits/places_cubit/places_cubit.dart';
 import 'package:sahayatri/cubits/destination_cubit/destination_cubit.dart';
 
 import 'package:sahayatri/ui/styles/styles.dart';
@@ -16,8 +17,18 @@ class TrackerFab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final destination = context.bloc<DestinationCubit>().destination;
+    return BlocBuilder<PlacesCubit, PlacesState>(
+      builder: (context, state) {
+        if (state is PlacesLoaded) {
+          return _buildFab(context);
+        }
+        return const Offstage();
+      },
+    );
+  }
 
+  Widget _buildFab(BuildContext context) {
+    final destination = context.bloc<DestinationCubit>().destination;
     return FloatingActionButton(
       backgroundColor: AppColors.dark,
       child: const Icon(
