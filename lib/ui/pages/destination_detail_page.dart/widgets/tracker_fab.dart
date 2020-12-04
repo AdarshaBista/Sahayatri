ce8c50c67e8a1671c71206f0d2a63e006b76f53e
AdarshaBista <adrsh07@gmail.com>
2020-12-04 09:23:31 +0545

    TrackerFab now depends on UserItineraryCubit

--- lib/ui/pages/destination_detail_page.dart/widgets/tracker_fab.dart
+++ lib/ui/pages/destination_detail_page.dart/widgets/tracker_fab.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:sahayatri/core/extensions/index.dart';
 import 'package:sahayatri/core/models/destination.dart';
 import 'package:sahayatri/core/services/navigation_service.dart';
 
 import 'package:sahayatri/app/constants/routes.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sahayatri/cubits/places_cubit/places_cubit.dart';
+import 'package:sahayatri/cubits/user_itinerary_cubit/user_itinerary_cubit.dart';
 
 import 'package:sahayatri/ui/styles/styles.dart';
-import 'package:sahayatri/ui/widgets/dialogs/message_dialog.dart';
 
 class TrackerFab extends StatelessWidget {
   const TrackerFab();
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PlacesCubit, PlacesState>(
+    return BlocBuilder<UserItineraryCubit, UserItineraryState>(
       builder: (context, state) {
-        if (state is PlacesLoaded) {
+        if (state is UserItineraryLoaded) {
           return _buildFab(context);
         }
         return const Offstage();
@@ -37,13 +35,6 @@ class TrackerFab extends StatelessWidget {
       ),
       onPressed: () {
         final destination = context.read<Destination>();
-        if (destination.createdItinerary == null) {
-          const MessageDialog(
-            message: 'You must create an itinerary before starting tracker.',
-          ).openDialog(context);
-          return;
-        }
-
         context.read<DestinationNavService>().pushNamed(
               Routes.trackerPageRoute,
               arguments: destination,
