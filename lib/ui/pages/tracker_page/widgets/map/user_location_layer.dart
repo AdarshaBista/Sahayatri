819e5cf81640eaa23f334ac70c7d5245190f3b42
AdarshaBista <adrsh07@gmail.com>
2021-03-23 00:36:41 +0545

    Fixed user marker making random jumps

--- lib/ui/pages/tracker_page/widgets/map/user_location_layer.dart
+++ lib/ui/pages/tracker_page/widgets/map/user_location_layer.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:latlong/latlong.dart';
-import 'package:sahayatri/core/models/coord.dart';
 import 'package:sahayatri/core/models/tracker_update.dart';
-import 'package:sahayatri/core/extensions/route_extension.dart';
 
 import 'package:provider/provider.dart';
 import 'package:sahayatri/ui/widgets/map/markers/animated_latlng.dart';
@@ -22,17 +20,10 @@ class UserLocationLayer extends StatelessWidget {
   Widget build(BuildContext context) {
     final trackerUpdate = context.watch<TrackerUpdate>();
     final userTrack = trackerUpdate.userTrack;
-
-    final zoom = Provider.of<double>(context, listen: false);
-    final coords = userTrack.map((t) => t.coord).toList().simplify(zoom);
+    final coords = userTrack.map((t) => t.coord).toList();
 
     final end = coords.last;
-    Coord begin;
-    if (coords.length >= 2) {
-      begin = coords[coords.length - 2];
-    } else {
-      begin = coords.first;
-    }
+    final begin = coords.length >= 2 ? coords[coords.length - 2] : coords.first;
 
     final child = AnimatedLatLng(
       begin: begin,
