ce89ac0d1c63f0fff8d0fddf0636958d411dbc9c
AdarshaBista <adrsh07@gmail.com>
2021-12-19 14:53:14 +0545

    Changed font from Sen to Poppins

--- lib/ui/styles/app_text_styles.dart
+++ lib/ui/styles/app_text_styles.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:sahayatri/core/constants/configs.dart';
 
 import 'package:sahayatri/ui/styles/app_colors.dart';
+import 'package:sahayatri/ui/styles/style_extension.dart';
 
 class AppTextStyles {
   AppTextStyles._();
@@ -12,35 +13,12 @@ class AppTextStyles {
     fontFamily: AppConfig.fontFamily,
   );
 
-  static final headline1 = _base.copyWith(
-    fontSize: 32.0,
-    fontWeight: FontWeight.w700,
-  );
-
-  static final headline2 = _base.copyWith(
-    fontSize: 25.0,
-    fontWeight: FontWeight.w600,
-  );
-
-  static final headline3 = _base.copyWith(
-    fontSize: 20.0,
-    fontWeight: FontWeight.w500,
-  );
-
-  static final headline4 = _base.copyWith(
-    fontSize: 17.0,
-    fontWeight: FontWeight.w400,
-  );
-
-  static final headline5 = _base.copyWith(
-    fontSize: 14.0,
-    color: AppColors.darkAccent,
-    fontWeight: FontWeight.w300,
-  );
-
-  static final headline6 = _base.copyWith(
-    fontSize: 11.0,
-    color: AppColors.darkFaded,
-    fontWeight: FontWeight.w200,
-  );
+  static final headline1 = _base.copyWith(fontSize: 32.0).bold;
+  static final headline2 = _base.copyWith(fontSize: 25.0).semiBold;
+  static final headline3 = _base.copyWith(fontSize: 20.0).medium;
+  static final headline4 = _base.copyWith(fontSize: 17.0).regular;
+  static final headline5 =
+      _base.copyWith(fontSize: 14.0, color: AppColors.darkAccent).thin;
+  static final headline6 =
+      _base.copyWith(fontSize: 11.0, color: AppColors.darkFaded).thin;
 }
--- lib/ui/styles/style_extension.dart
+++ lib/ui/styles/style_extension.dart
@@ -5,21 +5,31 @@ import 'package:sahayatri/core/constants/configs.dart';
 import 'package:sahayatri/ui/styles/app_colors.dart';
 
 extension TextStyleExtension on TextStyle {
-  TextStyle get thin => copyWith(fontWeight: FontWeight.w200);
+  TextStyle get thin => copyWith(fontWeight: FontWeight.w300);
+  TextStyle get regular => copyWith(fontWeight: FontWeight.w400);
+  TextStyle get medium => copyWith(fontWeight: FontWeight.w500);
+  TextStyle get semiBold => copyWith(fontWeight: FontWeight.w600);
   TextStyle get bold => copyWith(fontWeight: FontWeight.w700);
+
   TextStyle get primary => copyWith(color: AppColors.primary);
   TextStyle get primaryDark => copyWith(color: AppColors.primaryDark);
   TextStyle get primaryLight => withColor(AppColors.primary.withOpacity(0.25));
+
   TextStyle get secondary => copyWith(color: AppColors.secondary);
   TextStyle get secondaryDark => copyWith(color: AppColors.secondaryDark);
-  TextStyle get secondaryLight => withColor(AppColors.secondary.withOpacity(0.25));
+  TextStyle get secondaryLight =>
+      withColor(AppColors.secondary.withOpacity(0.25));
+
   TextStyle get light => copyWith(color: AppColors.light);
   TextStyle get lightFaded => copyWith(color: AppColors.lightFaded);
   TextStyle get lightAccent => copyWith(color: AppColors.lightAccent);
+
   TextStyle get dark => copyWith(color: AppColors.dark);
   TextStyle get darkFaded => copyWith(color: AppColors.darkFaded);
   TextStyle get darkAccent => copyWith(color: AppColors.darkFaded);
+
   TextStyle get serif => copyWith(fontFamily: AppConfig.fontFamilySerif);
+
   TextStyle withColor(Color color) => copyWith(color: color);
   TextStyle withSize(double size) => copyWith(fontSize: size);
 }
